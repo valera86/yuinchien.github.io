da74ab43d346c62ea631505de6f0bf31f685bcb9
Yuin Chien <tinyuin.chien@gmail.com>
2019-05-07 00:30:01 -0700

    updated index.js styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import resume from "../files/resume.pdf"
 export default ({ data }) => {
   return (
     <Layout>
-      <div id="projects">
+      <div id="projects" className="text">
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <span className="project" key={node.id}>
             <Link to={node.fields.slug}><span className="h1">{node.frontmatter.title}</span></Link>
@@ -15,7 +15,7 @@ export default ({ data }) => {
           </span>
         ))}
       </div>
-      <div className="h2">
+      <div className="h2 text">
         <div id="bio">Yuin Chien is a multidisciplinary designer who creates form and function by pushing pixels and code. Striving for aesthetics, imagination and intent, she explores the intersection of human interactions, design and technology. Her work has been selected by MoMA, Communication Arts, STA 100 Awards, Huffington Post, and The Verge. See <a href={resume}>resume</a>.</div>
         <div>● cloud@yuinchien.com</div>
         <div>● instagram.com/tinycloud</div>
