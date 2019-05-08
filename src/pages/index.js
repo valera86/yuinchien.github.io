1dca32242b583799ad82299a978143dfe9ddb514
Yuin Chien <tinyuin.chien@gmail.com>
2019-05-08 07:20:41 -0700

    don't style main

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,46 +2,49 @@ import React from "react"
 import { Link } from "gatsby"
 import { graphql } from 'gatsby'
 import resume from "../files/resume.pdf"
+import Layout from "./../components/layout"
 
 export default ({ data }) => {
   return (
-    <div id="home-wrapper">
-      <div id="home-container">
-        <div id="projects">
-          {data.allMarkdownRemark.edges.map(({ node }) => (
-            <span className="project" key={node.id}>
-              {node.frontmatter.redirect===""
-                ? (<Link to={node.fields.slug}><span className="h1">{node.frontmatter.title}</span></Link>)
-                : (<a href={node.frontmatter.redirect}><span className="h1">{node.frontmatter.title}</span></a>)
-              }
-              <span className="desc h1"> {node.frontmatter.description} ⁄ </span>
-            </span>
-          ))}
-        </div>
-        <div className="h2">
-          <div id="bio">Yuin Chien is a multidisciplinary designer who creates form and function by pushing pixels and code. Striving for aesthetics, imagination and intent, she explores the intersection of human interactions, design and technology. Her work has been selected by MoMA, Communication Arts, STA 100 Awards, Huffington Post, and The Verge. See <a href={resume}>resume</a>.</div>
-          <div>● cloud@yuinchien.com</div>
-          <div>● instagram.com/tinycloud</div>
-          <div>● twitter.com/yuinchien</div>
-        </div>
-        <div id="grid">
-          <div className="margin"></div>
-          <div id="col1" className="col"></div>
-          <div id="col2" className="col"></div>
-          <div id="col3" className="col"></div>
-          <div id="col4" className="col"></div>
-          <div id="col5" className="col"></div>
-          <div id="col6" className="col"></div>
-          <div id="col7" className="col"></div>
-          <div id="col8" className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="margin"></div>
+    <Layout>
+      <div id="home-wrapper">
+        <div id="home-container">
+          <div id="projects">
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <span className="project" key={node.id}>
+                {node.frontmatter.redirect===""
+                  ? (<Link to={node.fields.slug}><span className="h1">{node.frontmatter.title}</span></Link>)
+                  : (<a href={node.frontmatter.redirect}><span className="h1">{node.frontmatter.title}</span></a>)
+                }
+                <span className="desc h1"> {node.frontmatter.description} ⁄ </span>
+              </span>
+            ))}
+          </div>
+          <div className="h2">
+            <div id="bio">Yuin Chien is a multidisciplinary designer who creates form and function by pushing pixels and code. Striving for aesthetics, imagination and intent, she explores the intersection of human interactions, design and technology. Her work has been selected by MoMA, Communication Arts, STA 100 Awards, Huffington Post, and The Verge. See <a href={resume}>resume</a>.</div>
+            <div>● cloud@yuinchien.com</div>
+            <div>● instagram.com/tinycloud</div>
+            <div>● twitter.com/yuinchien</div>
+          </div>
+          <div id="grid">
+            <div className="margin"></div>
+            <div id="col1" className="col"></div>
+            <div id="col2" className="col"></div>
+            <div id="col3" className="col"></div>
+            <div id="col4" className="col"></div>
+            <div id="col5" className="col"></div>
+            <div id="col6" className="col"></div>
+            <div id="col7" className="col"></div>
+            <div id="col8" className="col"></div>
+            <div className="col"></div>
+            <div className="col"></div>
+            <div className="col"></div>
+            <div className="col"></div>
+            <div className="margin"></div>
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
--- src/templates/project.js
+++ src/templates/project.js
@@ -3,29 +3,30 @@ import { Link } from "gatsby"
 import { graphql } from 'gatsby'
 import Layout from "./../components/layout"
 
-
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
-      <div id="hero" className="section parallax">
-        <div style={{backgroundImage: `url(${post.frontmatter.cover.childImageSharp.fluid.src})`}} />
-      </div>
-      <div id="content" className="section">
-        <h1 className="text">{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div>
-      <div className="section parallax">
-        <div id="more-projects">
-          {data.allMarkdownRemark.edges.map(({ node }) => (
-            <div className={"project " + (node.fields.slug===post.fields.slug ? "line-thru": "") } key={node.id}>
-              {node.frontmatter.redirect===""
-                ? (<Link to={node.fields.slug}>{node.frontmatter.title}</Link>)
-                : (<a href={node.frontmatter.redirect}>{node.frontmatter.title}</a>)
-              }
-            </div>
-          ))}
-          <Link to="/" style={{marginTop:`24px`,display:`block`}}>—— yuinchien.com</Link>
+      <div id="project-wrapper">
+        <div id="hero" className="section parallax">
+          <div style={{backgroundImage: `url(${post.frontmatter.cover.childImageSharp.fluid.src})`}} />
+        </div>
+        <div id="content" className="section">
+          <h1 className="text">{post.frontmatter.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        </div>
+        <div className="section parallax">
+          <div id="more-projects">
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <div className={"project " + (node.fields.slug===post.fields.slug ? "line-thru": "") } key={node.id}>
+                {node.frontmatter.redirect===""
+                  ? (<Link to={node.fields.slug}>{node.frontmatter.title}</Link>)
+                  : (<a href={node.frontmatter.redirect}>{node.frontmatter.title}</a>)
+                }
+              </div>
+            ))}
+            <Link to="/" style={{marginTop:`24px`,display:`block`}}>—— yuinchien.com</Link>
+          </div>
         </div>
       </div>
     </Layout>
