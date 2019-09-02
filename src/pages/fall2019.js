25094f825a8e302c4c45dd528afe9fbf2684f573
Yuin Chien <tinyuin.chien@gmail.com>
2019-09-02 15:27:58 -0700

    add fall2019

---
+++ src/pages/fall2019.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Fall2019 = () => (
+  <Layout>
+    <SEO title="Interactive 1" />
+    <div id="teaching-wrapper">
+      <div id="home-container" className="animate-up">
+        <div>
+          <div className="teaching-topbar">
+            <div className="font-bold">Interactive 1</div>
+            <div><a rel="noopener noreferrer" href="https://paper.dropbox.com/doc/Interactive-1--AkApDAWzBUHC4vStHJmrUC4MAQ-eukDEaoHlRye93I9ra6w4">Syllabus</a></div>
+            <div><a rel="noopener noreferrer" href="https://paper.dropbox.com/doc/Readings-Doc--AkA8irVb8NkREt~yqN1wxHnGAQ-j14KACcM9txH5hMNn36Zn">Readings</a></div>
+          </div>
+          <br/>
+          <br/>
+          <div className="font-bold">2019 Fall</div>
+          <div className="teaching-desc">This course will explore the possibilities of design online from a conceptual, historical and programmatic perspective, from September 4 to December 13, 2019.</div>
+          <br/>
+          <div>
+            <div className="font-bold">California College of the Arts</div>
+            Fall 2019<br/>
+            Interactive 1<br/>
+            CCA GRAPH-2120-1<br/>
+            SF Main Campus, W2<br/>
+            Mon/Wed 4p–7p<br/>
+          </div>
+          <br/>
+          <div className="font-bold">Instructor</div>
+          <a rel="noopener noreferrer" href="https://yuinchien.com/" target="_blank">Yuin Chien</a>, <a rel="noopener noreferrer" href="mailto:yuin@cca.edu">yuin@cca.edu</a>
+        </div>
+      </div>
+    </div>
+  </Layout>
+)
+export default Fall2019
