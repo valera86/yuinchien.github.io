ac9f68dd239c2360215fa96c8eaa6813ad7414ff
Yuin Chien <tinyuin.chien@gmail.com>
2020-04-16 22:05:40 -0700

    consolidate images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Yuin Chien`,
-    description: `Yuin Chien is a San Francisco based designer and programmer focusing on graphic design & creative coding.`,
+    description: `Yuin Chien is a San Francisco based designer and programmer focusing on graphic design and creative coding`,
     author: `Yuin Chien`,
     url: "https://yuinchien.com",
     image: `/images/hi.png`,
