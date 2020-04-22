b2e4d220302bd8d7ac6ccd1ad7f4b7a0ac2d5331
Yuin Chien <tinyuin.chien@gmail.com>
2020-04-22 14:28:50 -0700

    updated about

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,7 @@ const About = () => (
       </div>
       <div id="personal-info" className="animate-up h1">
         <p>Yuin Chien is a San Francisco based designer and programmer focusing on graphic design, creative coding, risograph and plotter drawings. She is an Art Center alumni, and ex-googler for Google Creative Lab and Material Design. Her work has been selected by the MoMA Talk to Me exhibition, Communication Arts, STA 100 Awards, The Verge and TechCrunch.</p>
-        <p>Email <a rel="noopener noreferrer" href="mailto: cloud@yuinchien.com" target="_blank">cloud@yuinchien.com</a> for collaboration or say Hi! Checkout the online <a rel="noopener noreferrer" href="https://store.yuinchien.com" target="_blank">store</a>, and follow on <a rel="noopener noreferrer" href="https://www.instagram.com/yuin.chien/" target="_blank">Instagram</a> & <a rel="noopener noreferrer" href="https://twitter.com/yuinchien" target="_blank">Twitter</a> where she shares WIP and design process.</p>
+        <p>Email <a rel="noopener noreferrer" href="mailto: cloud@yuinchien.com" target="_blank">cloud@yuinchien.com</a> for collaboration or say Hi! Checkout the online <a rel="noopener noreferrer" href="https://store.yuinchien.com" target="_blank">store</a>, and keep in touch on <a rel="noopener noreferrer" href="https://www.instagram.com/yuin.chien/" target="_blank">Instagram</a> & <a rel="noopener noreferrer" href="https://twitter.com/yuinchien" target="_blank">Twitter</a> where she shares WIP and design process.</p>
       </div>
     </div>
   </Layout>
