82e91967e37841558d37fbe062d8b6b8a578ee7a
Yuin Chien <tinyuin.chien@gmail.com>
2020-05-05 19:35:54 -0700

    updated nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,32 +5,68 @@ import resume from "../files/resume.pdf"
 import Layout from "./../components/layout"
 import SEO from "../components/seo"
 
-export default ({ data }) => {
-  return (
-    <Layout>
-      <SEO title="Projects" />
-      <div id="home-container" >
-        <div id="nav">
-          <Link to="/" target="_blank" className="selected">Projects</Link>
-          <Link to="/about" target="_blank">About</Link>
-          <a rel="noopener noreferrer" href={resume} target="_blank">Resume</a>
-          <a rel="noopener noreferrer" href="https://store.yuinchien.com" target="_blank">Store</a>
-        </div>
-        <div id="projects" className="animate-up">
-          {data.allMarkdownRemark.edges.map(({ node }) => (
-            <div className="project" key={node.id}>
-              <Link to={node.fields.slug}><img alt="hero" src={node.frontmatter.cover.childImageSharp.fluid.src} /></Link>
-              <div className="info">
-                <span>{node.frontmatter.title}</span>
-                <span className="desc"> {node.frontmatter.description} </span>
+class Index extends React.Component  {
+
+  constructor(props) {
+    super(props);
+    this.lastScroll = 0;
+    this.state = {
+      scrollUp: false
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = (event) => {
+    const currentScroll = window.scrollY;
+    if (currentScroll === 0) {
+      this.setState({scrollUp: false});
+      return;
+    }
+    if (currentScroll < this.lastScroll && this.state.scrollUp) {
+      this.setState({scrollUp: false});
+    } else if (currentScroll > this.lastScroll && !this.state.scrollUp) {
+      this.setState({scrollUp: true});
+    }
+    this.lastScroll = currentScroll;
+  }
+
+  render() {
+    const { data } = this.props;
+    return (
+      <Layout>
+        <SEO title="Projects" />
+        <div id="home-container">
+          <div id="nav" className={ this.state.scrollUp ? "scroll-top" :"" }>
+            <Link to="/" target="_blank" className="selected">Projects</Link>
+            <Link to="/about" target="_blank">About</Link>
+            <a rel="noopener noreferrer" href={resume} target="_blank">Resume</a>
+            <a rel="noopener noreferrer" href="https://store.yuinchien.com" target="_blank">Store</a>
+          </div>
+          <div id="projects" className="animate-up">
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <div className="project" key={node.id}>
+                <Link to={node.fields.slug}><img alt="hero" src={node.frontmatter.cover.childImageSharp.fluid.src} /></Link>
+                <div className="info">
+                  <span>{node.frontmatter.title}</span>
+                  <span className="desc"> {node.frontmatter.description} </span>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </Layout>
-  )
+      </Layout>
+    )
+  }
 }
+
+export default Index;
 
 export const query = graphql`
   query {
