267697b44efc2ec2c61cbdfb4b8b426e6d4154f7
Yuin Chien <tinyuin.chien@gmail.com>
2020-05-11 11:53:23 -0700

    updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,39 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+class ComponentIframe extends React.Component  {
+  iframe() {
+    return {
+      __html: this.props.iframe
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div dangerouslySetInnerHTML={ this.iframe() } />
+      </div>
+    );
+  }
+}
+
+const iframe = '<iframe src="https://yuinchien.com/projects/variable-font/404" width="100%" style="position:absolute;width:100%;top:0;z-index:-1;height: 100vh"></iframe>';
+
+class NotFoundPage extends React.Component  {
+  render() {
+    return (
+      <Layout>
+        <SEO title="404: Not found" />
+        <div id="home-container">
+          <div id="nav">
+            <Link to="/">yuinchien.com</Link>
+          </div>
+        </div>
+        <ComponentIframe iframe={iframe} />
+      </Layout>
+    )
+  }
+}
 
 export default NotFoundPage
