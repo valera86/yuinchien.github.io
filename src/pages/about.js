48c7216f10a2ca10e68caef7b2d2221406e0b882
Yuin Chien <tinyuin.chien@gmail.com>
2020-08-20 19:30:33 -0700

    updated about

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ const About = () => (
       <Header selected="about" />
       <div id="personal-info" className="animate-up">
         <div id="svg-about"></div>
-        <p class="h4">Email <a rel="noopener noreferrer" href="mailto: cloud@yuinchien.com" target="_blank">cloud@yuinchien.com</a> for collaboration or say Hi! Please checkout the online <a rel="noopener noreferrer" href="https://store.yuinchien.com" target="_blank">store</a> for her printmaking projects, and keep in touch on <a rel="noopener noreferrer" href="https://www.instagram.com/yuin.chien/" target="_blank">Instagram</a> and <a rel="noopener noreferrer" href="https://twitter.com/yuinchien" target="_blank">Twitter</a> where she shares WIP and design process.</p>
+        <p className="h4">Email <a rel="noopener noreferrer" href="mailto: cloud@yuinchien.com" target="_blank">cloud@yuinchien.com</a> for collaboration or say Hi! Please checkout the online <a rel="noopener noreferrer" href="https://store.yuinchien.com" target="_blank">store</a> for her printmaking projects, and keep in touch on <a rel="noopener noreferrer" href="https://www.instagram.com/yuin.chien/" target="_blank">Instagram</a> and <a rel="noopener noreferrer" href="https://twitter.com/yuinchien" target="_blank">Twitter</a> where she shares WIP and design process.</p>
       </div>
     </div>
   </Layout>
